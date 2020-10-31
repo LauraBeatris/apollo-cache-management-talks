d4bc524d84f546b6458601cd4b612735aafaefd1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-10-31 12:03:48 -0300

    Add first slides

---
+++ src/components/SocialMediaHandle/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { Container, IconContainer } from "./styles";
+
+const socialMedias = [
+  {
+    icon: <FaTwitter fill="#1DA1F2"/>,
+    user: "@lauradotjs"
+  },
+  {
+    icon: <FaLinkedin fill="#1DA1F2" />,
+    user: "LauraBeatris"
+  },
+  {
+    icon: <FaGithub fill="#00000" />,
+    user: "LauraBeatris"
+  }
+]
+
+const SocialMediaHandle = () => (
+  <Container>
+    {
+      socialMedias.map(socialMedia => (
+        <IconContainer>
+          {socialMedia.icon}
+          <span>{socialMedia.user}</span>
+        </IconContainer>
+      ))
+    }
+  </Container>
+);
+
+export default SocialMediaHandle;
---
+++ src/components/SocialMediaHandle/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 20%; 
+  display: grid;
+  grid-column-gap: 15px;
+  grid-template-columns: repeat(3, auto);
+  margin-bottom: 5px;
+`;
+
+export const IconContainer = styled.div`
+  display: flex; 
+  align-items: center;
+
+  span { 
+    font-weight: bold; 
+    margin-left: 8px;
+  }
+
+  span, svg { 
+    font-size: 20px;
+  }  
+`;
